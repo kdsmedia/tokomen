4325bdc8eab1fd226b9e7f525d24686dc60ffe21
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-24 14:34:38 +0700

    Update start_comments.py

--- start_comments.py
+++ start_comments.py
@@ -1,7 +1,6 @@
 import time
 from flask import Flask, request, jsonify
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from faker import Faker
